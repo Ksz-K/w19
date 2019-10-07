dc350bcad7b8153459d12e69b0a188a59cf0f5a1
Ksz-K <52027829+Ksz-K@users.noreply.github.com>
2019-10-07 09:46:31 +0200

    Tunning

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ Date.now() % 2 == 0
 //Exercise 2 - trochę po polsku
 let a = prompt("Podaj mnożną");
 let b = prompt("Podaj mnożnik");
-const multiply = (mnozna, mnoznik = 1) => {
-  console.log(mnozna * mnoznik);
+const multiply = (multiplicand, multiplier = 1) => {
+  console.log(multiplicand * multiplier);
 };
 b !== "" && !isNaN(b) ? multiply(a, b) : multiply(a);
 
@@ -22,6 +22,11 @@ const average = (...numbers) => {
   console.log(sum / numbers.length);
 };
 average.apply(this, arr);
+const averageSecond =
+  arr.reduce((total, element) => {
+    return total + element;
+  }, 0) / arr.length;
+console.log("Sprawdzenie wyniku średniej " + averageSecond);
 //average(arr); //to nie działa
 //average(1, 2, 3, 4, 5, 6, 7); //to działa
 
